c8e022a3434f3d952f66ca148930b4adea2605df
Yan Marques de Cerqueira <yan.cerqueira@unisul.br>
2020-10-06 17:24:26 -0300

    Add configuration parsing from command line

--- unisul_sync_gui/cli.py
+++ unisul_sync_gui/cli.py
@@ -1,5 +1,26 @@
 import unisul_sync_gui
 
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    
+    subparsers = parser.add_subparsers()
+    cfg_parser = subparsers.add_parser('config')
+    cfg_parser.add_argument('--get', 
+                            metavar='KEY', 
+                            help='Retrieve a configuration parameter.')
+
+    return parser.parse_args()
+
 
 def entrypoint():
-    unisul_sync_gui.main()
+    args = parse_args()
+    if 'get' in args:
+        # get configuration
+        config = unisul_sync_gui.config.load()
+        if args.get in config:
+            print(config[args.get])
+    else:
+        # start app
+        unisul_sync_gui.main()
