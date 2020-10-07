7aefbd5bd09be7e0b13809354ce2ac3aeb59c4c9
Yan Marques de Cerqueira <yan.cerqueira@unisul.br>
2020-10-07 19:54:38 -0300

    Remove unecessary submodules collection

--- winbuild/hooks/hook-unisul_sync_gui.py
+++ winbuild/hooks/hook-unisul_sync_gui.py
@@ -1,6 +1,3 @@
-from PyInstaller.utils.hooks import collect_data_files, collect_submodules
+from PyInstaller.utils.hooks import collect_submodules
 
-hiddenimports = (
-    collect_submodules('unisul_sync_gui') +
-    collect_submodules('unisul_sync_gui.book_bot')
-)
+hiddenimports = collect_submodules('unisul_sync_gui')
