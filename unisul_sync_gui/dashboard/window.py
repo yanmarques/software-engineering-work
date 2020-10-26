35c374ef24e53e306ba0aca94c759f6e0428826e
Yan Marques de Cerqueira <yan.cerqueira@unisul.br>
2020-10-26 17:22:02 -0300

    Improve logout action using threads

--- unisul_sync_gui/dashboard/window.py
+++ unisul_sync_gui/dashboard/window.py
@@ -1,5 +1,6 @@
 from . import screen
 from .. import config, widgets
+from ..builtin_plugins import util
 from ..app import context
 from PyQt5 import QtWidgets
 
@@ -19,6 +20,9 @@ class Dashboard(QtWidgets.QMainWindow, screen.Ui_MainWindow):
         self.opts_menu.addAction(self.close_button)
         self.tabWidget.setCurrentIndex(context.config.get('default_tab', 0))
 
+        # use here to store logout qthread
+        self.logout_runner = None
+
         self.show()
         context.signals.landed.emit(sender=self)
 
@@ -26,13 +30,18 @@ class Dashboard(QtWidgets.QMainWindow, screen.Ui_MainWindow):
         msg = widgets.ConfirmationMessageBox(default_accept=False)
         msg.setText('Tem certeza que deseja sair?')
         if msg.is_accepted():
-            login = context.windows['login'] 
-            
-            # actully logout, this invalidates the cookiejar stored in disk
-            login.http_auth.logout()
+            login = context.windows['login']
 
-            self.close()
+            def on_logout_done():
+                self.close()
 
-            # reopen a fresh login window, without trying credentials from disk
-            # because we do not want to login again
-            context.windows['login'] = login.new_from_this(ignore_disk_creds=True)
+                # reopen a fresh login window, without trying credentials from disk
+                # because we do not want to login again
+                context.windows['login'] = login.new_from_this(ignore_disk_creds=True)
+
+            self.setDisabled(True)
+
+            # will actually logout, this invalidates the cookiejar stored in disk
+            self.logout_runner = util.GenericCallbackRunner(login.http_auth.logout)
+            self.logout_runner.done.connect(on_logout_done)
+            self.logout_runner.start()
