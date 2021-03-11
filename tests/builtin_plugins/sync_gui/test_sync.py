b6758f6238d29f4c029bbf7ccc29002376c08877
Yan Marques de Cerqueira <marques_yan@outlook.com>
2021-03-11 19:02:20 -0300

    implement initial code for crawling a spider

---
+++ tests/builtin_plugins/sync_gui/test_sync.py
@@ -0,0 +1,25 @@
+from unisul_sync_gui.crawler import (
+    signal, 
+    cookie,
+    MiddlewareAwareCrawler,
+)
+from unisul_sync_gui.builtin_plugins.sync_gui.crawler import exporter
+from unisul_sync_gui.builtin_plugins.sync_gui import spiders
+
+
+async def implementation():
+    async def on_subjects_done(subjects):
+        e = exporter.SubjectExporter('test-subjects.json')
+        e.export(subjects)
+        subjects = e.dumper.load()
+
+    s = signal.CrawlerSignals()
+    s.on_processed_response.connect(on_subjects_done)
+
+    m = signal.SignalingMiddleware(spiders.SubjectSpider(), s)
+
+    def session_factory():
+        return cookie.ClientSession('foo.cookie')
+
+    c = MiddlewareAwareCrawler(m, session_factory=session_factory)
+    c.start()
